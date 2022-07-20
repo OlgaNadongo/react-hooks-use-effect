b3b9772f02e866821e8027bd9ac5a97acbef7066
Olga Nadongo <winnienadongo@gmail.com>
2022-07-20 11:40:37 +0300

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+
+   const [count, setCount]=useState(0)
+   const [text, setText]=useState("")
+
+   function handleClick(){
+         setCount((count)=>count+1)
+   }
+
+   function handleChange(e){
+       setText(e.target.value)
+   }
+
+
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text; 
+  }, [text]);
+ 
+useEffect(()=>{
+  console.log("hey");
+  setTimeout(()=>setCount(0),5000)
+},[])
 
-  console.log("Component rendering");
+console.log("Component rendering");
 
-  return <button>Click Me</button>;
+//return JSX
+  return (
+      <div>
+          <button 
+          value={count} 
+          onClick={handleClick}>
+             I've been clicked {count} times
+            </button>;
+
+
+          <input 
+          type="text" 
+          placeholder="Type away..." 
+          value={text} 
+          onChange={handleChange} />
+      </div>
+          )
+  
 }
+    
+    
 
 export default App;
